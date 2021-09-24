2214f7b7cb40d6fe01f250c3cc2becd45d4837c1
Elvis Alam <88417025+theelvisalam@users.noreply.github.com>
2021-09-24 18:18:55 -0500

    functions section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,204 +1,204 @@
 'use strict';
 
-// const bookings = [];
+const bookings = [];
 
-// const createBooking = function (
-//   flightNum,
-//   numPassengers = 1,
-//   price = 199 * numPassengers
-// ) {
-//   // // ES5
-//   //   numPassengers = numPassengers || 1;
-//   //   price = price || 199;
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
 
-//   const booking = {
-//     flightNum,
-//     numPassengers,
-//     price,
-//   };
-//   console.log(booking);
-//   bookings.push(booking);
-// };
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
 
-// createBooking('LH123');
-// createBooking('LH123', 2, 800);
-// createBooking('LH123', 2);
-// createBooking('LH123', 5);
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
 
-// createBooking('LH123', undefined, 1000);
+createBooking('LH123', undefined, 1000);
 
-// const flight = 'LH234';
-// const jonas = {
-//   name: 'Jonas Schmedtmann',
-//   passport: 24739479284,
-// };
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
 
-// const checkIn = function (flightNum, passenger) {
-//   flightNum = 'LH999';
-//   passenger.name = 'Mr. ' + passenger.name;
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
 
-//   if (passenger.passport === 24739479284) {
-//     alert('Checked in');
-//   } else {
-//     alert('Wrong passport!');
-//   }
-// };
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
 
-// checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
 
-// IS THE SAME AS DOING...
-// const flightNum = flight;
-// const passenger = jonas;
+IS THE SAME AS DOING...
+const flightNum = flight;
+const passenger = jonas;
 
-// const newPassport = function (person) {
-//   person.passport = Math.trunc(Math.random() * 100000000);
-// };
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
 
-// newPassport(jonas);
-// checkIn(flight, jonas);
+newPassport(jonas);
+checkIn(flight, jonas);
 
-// const oneWord = function (str) {
-//   return str.replace(/ /g, '').toLowerCase();
-// };
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
 
-// const upperFirstWord = function (str) {
-//   const [first, ...others] = str.split(' ');
-//   return [first.toUpperCase(), ...others].join(' ');
-// };
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
 
-// // Higher-order function
-// const transformer = function (str, fn) {
-//   console.log(`Original string ${str}`);
-//   console.log(`Transformed string: ${fn(str)}`);
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
 
-//   console.log(`Transformed by: ${fn.name}`);
-// };
+  console.log(`Transformed by: ${fn.name}`);
+};
 
-// transformer('JavaScript is the best!', upperFirstWord);
-// transformer('JavaScript is the best!', oneWord);
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
 
-// // JS uses callbacks all the time
-// const high5 = function () {
-//   console.log('high five');
-// };
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('high five');
+};
 
-// document.body.addEventListener('click', high5);
+document.body.addEventListener('click', high5);
 
-// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
 
-// const greet = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting} ${name}`);
-//   };
-// };
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
 
-// const greet2 = greeting => name => console.log(`${greeting} ${name}`);
+const greet2 = greeting => name => console.log(`${greeting} ${name}`);
 
-// const greeterHey = greet2('Hey');
-// greeterHey('Jonas');
-// greeterHey('Steven');
+const greeterHey = greet2('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
 
-// greet2('Hello')('Jonas');
+greet2('Hello')('Jonas');
 
-// const lufthansa = {
-//   airline: 'Lufthansa',
-//   iataCode: 'LH',
-//   bookings: [],
-//   // book: function(){},
-//   book(flightNum, name) {
-//     console.log(
-//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-//     );
-//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-//   },
-// };
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function(){},
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
 
-// lufthansa.book(239, 'Jonas Schmedtmann');
-// lufthansa.book(239, 'John Smith');
-// console.log(lufthansa);
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(239, 'John Smith');
+console.log(lufthansa);
 
-// const eurowings = {
-//   name: 'Eurowings',
-//   iataCode: 'EW',
-//   bookings: [],
-// };
+const eurowings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
 
-// const book = lufthansa.book;
+const book = lufthansa.book;
 
-// // DOES NOT WORK
-// // book(23, 'Sarah Williams');
+// DOES NOT WORK
+// book(23, 'Sarah Williams');
 
-// // CALL METHOD
+// CALL METHOD
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// APPLY METHOD
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// BIND METHOD
 // book.call(eurowings, 23, 'Sarah Williams');
-// console.log(eurowings);
 
-// book.call(lufthansa, 239, 'Mary Cooper');
-// console.log(lufthansa);
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
 
-// const swiss = {
-//   airline: 'Swiss Air Lines',
-//   iataCode: 'LX',
-//   bookings: [],
-// };
+bookEW(23, 'Steven Williams');
 
-// book.call(swiss, 583, 'Mary Cooper');
-// console.log(swiss);
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
 
-// // APPLY METHOD
-// const flightData = [583, 'George Cooper'];
-// book.apply(swiss, flightData);
-// console.log(swiss);
+// WITH EVENT LISTENERS
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
 
-// book.call(swiss, ...flightData);
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
 
-// // BIND METHOD
-// // book.call(eurowings, 23, 'Sarah Williams');
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
-// const bookEW = book.bind(eurowings);
-// const bookLH = book.bind(lufthansa);
-// const bookLX = book.bind(swiss);
+// PARTIAL APPLICATION
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
 
-// bookEW(23, 'Steven Williams');
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = (rate, value) => value + value * 0.23;
 
-// const bookEW23 = book.bind(eurowings, 23);
-// bookEW23('Jonas Schmedtmann');
-// bookEW23('Martha Cooper');
+console.log(addVAT(100));
+console.log(addVAT(23));
 
-// // WITH EVENT LISTENERS
-// lufthansa.planes = 300;
-// lufthansa.buyPlane = function () {
-//   console.log(this);
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
 
-//   this.planes++;
-//   console.log(this.planes);
-// };
-// // lufthansa.buyPlane();
-
-// document
-//   .querySelector('.buy')
-//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-
-// // PARTIAL APPLICATION
-// const addTax = (rate, value) => value + value * rate;
-// console.log(addTax(0.1, 200));
-
-// const addVAT = addTax.bind(null, 0.23);
-// // addVAT = (rate, value) => value + value * 0.23;
-
-// console.log(addVAT(100));
-// console.log(addVAT(23));
-
-// const addTaxRate = function (rate) {
-//   return function (value) {
-//     return value + value * rate;
-//   };
-// };
-
-// const addVAT2 = addTaxRate(0.23);
-// console.log(addVAT2(100));
-// console.log(addVAT2(23));
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
 
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -239,4 +239,135 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // GET ANSWER
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // REGISTER ANSWER
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
 };
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+// EXAMPLE 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// RE-ASSIGNING F FUNCTION
+h();
+f();
+console.dir(f);
+
+// EXAMPLE 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
